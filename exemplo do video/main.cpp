112a8856f24104a1e4bcf2782c8240140d83a345
EtecVAV-1D-EtecNerds <grupoetec51@gmail.com>
2025-08-18 11:08:44 -0300

    exercicio do video

---
+++ exemplo do video/main.cpp
@@ -0,0 +1,44 @@
+# include <stdio.h>
+# include <string>
+#include <iostream>
+#include <stdlib.h>
+
+using namespace std;
+
+int main(int argc, char** argv) {
+    string linha;
+    int primo;
+    bool isPrimo;
+
+    while(true)
+    {
+        getline(cin, linha);
+
+        if(linha == "0")
+        {
+            break;
+        }
+
+        primo = atoi(linha.c_str());
+        isPrimo = true;
+
+        for(int i = 2; i <= primo; i++)
+        {
+            if(((primo % i) == 0) && (i != primo))
+            {
+                isPrimo = false;
+                break;
+            }
+        }
+
+        if(primo > 1)
+        {
+            if(isPrimo)
+            {
+                cout << primo;
+            }
+        }
+    }
+return 0;
+}
+
